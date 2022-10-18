90c5ba9d72a37a2d27043ef6f92b2a735bfde8c4
alikarimi <karimi.me.ali@gmail.com>
2022-10-18 22:28:13 +0330

    add popup div and styles

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static("public")) 
+app.use(express.static("public"))
 
 app.use(express.urlencoded({extended: true}))  // this allows us to access information coming from form
 app.use(express.json())   // when posting json information fetch from client to sever
